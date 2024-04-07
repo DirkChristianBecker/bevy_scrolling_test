b03e6a878242ed3a6c2d4c2858c8cc41f2da53d7
DirkChristianBecker <121241657+DirkChristianBecker@users.noreply.github.com>
2024-04-07 10:58:09 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+mod plugin;
+
+pub mod prelude {
+    use crate::plugin::PluginTemplate;
+
+    pub use crate::plugin::PluginTemplate;
+}
---
+++ src/plugin.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+
+pub struct PluginTemplate;
+impl Plugin for PluginTemplate {
+    fn build(&self, app: &mut App) {
+        
+    }
+}
